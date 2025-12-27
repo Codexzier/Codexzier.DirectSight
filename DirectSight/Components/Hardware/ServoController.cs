70bf96a4b9788d60dee19f614ab0eb93bd287987
Codexzier <johannes.paul.langner@live.de>
2025-12-27 12:22:23 +0100

    start instance of servo controller

---
+++ DirectSight/Components/Hardware/ServoController.cs
@@ -0,0 +1,47 @@
+﻿using System.Device.Pwm;
+
+namespace DirectSight.Components.Hardware;
+
+    public sealed class ServoController : IDisposable
+    {
+        private readonly PwmChannel _servo0;
+        private readonly PwmChannel _servo1;
+
+        public ServoController()
+        {
+            // Hardware PWM via Linux
+            this._servo0 = PwmChannel.Create(
+                chip: 0,
+                channel: 0,   // GPIO 12
+                frequency: 50,
+                dutyCyclePercentage: 0.075);
+
+            this._servo1 = PwmChannel.Create(
+                chip: 0,
+                channel: 1,   // GPIO 13
+                frequency: 50,
+                dutyCyclePercentage: 0.075);
+
+            this._servo0.Start();
+            this._servo1.Start();
+        }
+
+        public void SetServo0(double duty)
+        {
+            this._servo0.DutyCycle = duty;
+        }
+
+        public void SetServo1(double duty)
+        {
+            this._servo1.DutyCycle = duty;
+        }
+
+        public void Dispose()
+        {
+            this._servo0?.Stop();
+            this._servo1?.Stop();
+            this._servo0?.Dispose();
+            this._servo1?.Dispose();
+        }
+    }
+
--- DirectSight/Worker.cs
+++ DirectSight/Worker.cs
@@ -1,11 +1,16 @@
+using DirectSight.Components.Hardware;
+
 namespace DirectSight;
 
 public class Worker(ILogger<Worker> logger) : BackgroundService
 {
+    private ServoController? _servos;
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         TcpTestServer.StartAsync(stoppingToken, logger);
         DataExchangeServer.StartAsync(stoppingToken, logger);
+        this._servos = new ServoController();
         
         
         while (!stoppingToken.IsCancellationRequested)
@@ -14,6 +19,10 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
             {
                 logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
+            
+            // Test: Move servos back and forth
+            this._servos.SetServo0(0.05);
+            this._servos.SetServo1(0.10);
 
             await Task.Delay(1000, stoppingToken);
         }
